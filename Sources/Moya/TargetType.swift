ff5444803ab645970e6e2b53831ed99c903d480c
Madushan Gamage <madushanbsns@gmail.com>
2017-01-22 10:12:24 +0800

    proposal to split TargetType Protocol

--- Sources/Moya/TargetType.swift
+++ Sources/Moya/TargetType.swift
@@ -1,35 +1,59 @@
 import Foundation
 import Alamofire
 
-/// The protocol used to define the specifications necessary for a `MoyaProvider`.
-public protocol TargetType {
+/// The protocol used to define the full specifications necessary for a `MoyaProvider`.
+public protocol TargetType: TargetURLType, TargetHTTPMethodType, TargetParametersType, TargetSampleDataType, TargetTaskType, TargetValidationType {}
 
-    /// The target's base `URL`.
-    var baseURL: URL { get }
+/// The protocol used to define the 'baseURL' and 'path' specifications.
+public protocol TargetURLType {
 
-    /// The path to be appended to `baseURL` to form the full `URL`.
-    var path: String { get }
+  /// The target's base `URL`.
+  var baseURL: URL { get }
 
-    /// The HTTP method used in the request.
-    var method: Moya.Method { get }
-
-    /// The parameters to be incoded in the request.
-    var parameters: [String: Any]? { get }
-
-    /// The method used for parameter encoding.
-    var parameterEncoding: ParameterEncoding { get }
-
-    /// Provides stub data for use in testing.
-    var sampleData: Data { get }
-
-    /// The type of HTTP task to be performed.
-    var task: Task { get }
-
-    /// Whether or not to perform Alamofire validation. Defaults to `false`.
-    var validate: Bool { get }
+  /// The path to be appended to `baseURL` to form the full `URL`.
+  var path: String { get }
 }
 
-public extension TargetType {
+/// The protocol used to define the HTTP request method specifications.
+public protocol TargetHTTPMethodType {
+
+  /// The HTTP method used in the request.
+  var method: Moya.Method { get }
+}
+
+/// The protocol used to define the parameter and encoding specifications.
+public protocol TargetParametersType {
+
+  /// The parameters to be incoded in the request.
+  var parameters: [String: Any]? { get }
+
+  /// The method used for parameter encoding.
+  var parameterEncoding: ParameterEncoding { get }
+}
+
+/// The protocol used to define the sample data specifications.
+public protocol TargetSampleDataType {
+
+  /// Provides stub data for use in testing.
+  var sampleData: Data { get }
+}
+
+/// The protocol used to define the HTTP task type specifications.
+public protocol TargetTaskType {
+
+  /// The type of HTTP task to be performed.
+  var task: Task { get }
+}
+
+/// The protocol used to define the Alamofire validation requirements.
+public protocol TargetValidationType {
+
+  /// Whether or not to perform Alamofire validation. Defaults to `false`.
+  var validate: Bool { get }
+}
+
+
+public extension TargetValidationType {
     var validate: Bool {
         return false
     }
